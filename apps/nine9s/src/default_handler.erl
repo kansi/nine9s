b454726ff8f78198dc566ad4202c8d614e6076b7
kansi <kansi13@gmail.com>
2015-11-28 01:02:39 +0530

    nine9s: version 0.1.0

---
+++ apps/nine9s/src/default_handler.erl
@@ -0,0 +1,8 @@
+-module(default_handler).
+-export([init/2]).
+
+init(Req, Opts) ->
+    Req2 = cowboy_req:reply(200, [ {<<"content-type">>, 
+                                    <<"text/plain">>} ], 
+                            <<"Hello world!">>, Req), 
+    {ok, Req2, Opts}.
